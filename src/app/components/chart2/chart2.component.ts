58d57ef61272dfd79a710791cc1ce94a0d33d428
quangpt97 <emotionbot1234@gmail.com>
2018-10-30 17:04:34 +0700

    change padding chart2

--- src/app/components/chart2/chart2.component.ts
+++ src/app/components/chart2/chart2.component.ts
@@ -334,7 +334,7 @@ export class Chart2Component implements OnInit {
               zeroLineWidth: 1,
               zeroLineColor: '#f5f4f0',
               color: '#f5f4f0',
-              tickMarkLength: 25,
+              tickMarkLength: 40,
               drawTicks: true
             },
           }],
