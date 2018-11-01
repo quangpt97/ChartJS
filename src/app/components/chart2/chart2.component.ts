eccc108bba2394f84360afe15d7fefcd898b9b08
quangpt97 <emotionbot1234@gmail.com>
2018-11-01 15:40:05 +0700

    print Chart

--- src/app/components/chart2/chart2.component.ts
+++ src/app/components/chart2/chart2.component.ts
@@ -335,7 +335,7 @@ export class Chart2Component implements OnInit {
           yAxes: [{
             ticks: {
               beginAtZero: true,
-              padding: 20,
+              padding: 30,
               callback: function (value) {
                 return '    ' + value + '件';
               },
@@ -347,7 +347,7 @@ export class Chart2Component implements OnInit {
               zeroLineWidth: 1,
               zeroLineColor: '#f5f4f0',
               color: '#f5f4f0',
-              tickMarkLength: 30,
+              tickMarkLength: 35,
               drawTicks: true
             },
           }],
